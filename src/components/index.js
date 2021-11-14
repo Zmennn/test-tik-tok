8a07666a5d425f39e8615b27ca7a20ca85c568ec
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-11-14 20:32:04 +0200

    0.22

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { AddInformation } from "./AddInformation";
 export { Hashtags } from "./Hashtags";
-export { Author } from "./Author";
+export { Author } from "./Author/Author";
 export { UserInfo } from "./UserInfo";
 export { Video } from "./Video";
 export { Gallery } from "./Gallery";
