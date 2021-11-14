88f310fe608f906fb049d4009599cb7e9e4c1b4b
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-11-14 16:10:57 +0200

    Update Video.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,7 +1,16 @@
 
 
 export function Video({ url }) {
+
+    const clickHandler = (event) => {
+        if (event.target.tagName !== "VIDEO") { return };
+        if(event.target.paused){    
+            event.target.play()   
+        }else{event.target.pause()}
+    }
     return (<video
+        // controls="controls"
+        onClick={clickHandler}
     src={url}
     >
   
