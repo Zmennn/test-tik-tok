ae2d2a116f62bbd0ffb8bcf8cdd7d398a4591e75
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-11-17 19:33:05 +0200

    1.3

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -2,14 +2,21 @@ import style from './style.module.css';
 
 export function Pagination({ isNumberPage }) {
     
-    return [1, 2, 3].map((number) => (
-        <button
-            key={number}
-            id={number}
-            className={style.button}
-            type="button"
-            onClick={() => isNumberPage(number)}
-        >{number}
-        </button>
-    ))
+    return (
+        <div className={style.container}>
+{
+        [1, 2, 3].map((number) => (
+            <button
+                key={number}
+                id={number}
+                className={style.button}
+                type="button"
+                onClick={() => isNumberPage(number)}
+            >{number}
+            </button>
+        ))
+    }
+        </div>
+    )
+    
 }
